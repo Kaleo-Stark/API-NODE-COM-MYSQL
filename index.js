30e83a5691bf636ee13a38115adf024a9a724383
Kaleo-Stark <kaleovieira.dev@gmail.com>
2021-07-27 22:03:59 -0300

    Rota de pesquisa de cliente por id

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // ###########( CONFIGURAÇÕES )###########
 const express = require('express') ; // ... Importa a biblioteca Express, responsavel pelos recursos WEBService.
 const   app   =     express()      ; // ... Cria um estância do express;
@@ -6,7 +5,7 @@ const  mysql  =  require('mysql')  ; // ... Importa a biblioteca do MySQL.
 const router  =  express.Router()  ; // ... Estância uma nova rota express;
 //const cors  =  require('cors') ; // ... Responsável por controlar o acesso a API.
 
-//app.use   (     cors()     ) ; // ......... Configura a API para aceitar requisições de qualquer origem com o cors.
+//app.use   (     cors()     ) ; // ....... Configura a API para aceitar requisições de qualquer origem com o cors.
 app.use   ( express.json() ) ; // ......... Configura a API para trabalhar com JSON.
 app.listen(      3333      ) ; // ......... Faz a API escutar a porta '3000' do servidor da API.
 // ####################################### 
@@ -14,13 +13,23 @@ app.listen(      3333      ) ; // ......... Faz a API escutar a porta '3000' do 
 
 // ###############( ROTAS )###############
 
-router.get('/', (req, res) => { // ........ Define uma rota raiz;
-    return res.json( { message: 'Funcionando' } );  // Retorna uma mensagem ao acessar essa rota com o metodo get.
+router.get('/', (req, res) => { // ...................... Define uma rota raiz;
+    return res.json( { message: 'Funcionando' } ); // ... Retorna uma mensagem ao acessar essa rota com o metodo get.
 });
 
-router.get('/clientes', (req, res) => {
-    execSQLQuery('SELECT * FROM Clientes', res);
+router.get('/clientes', (req, res) => { // ............ Define uma rota GET para 'clientes', onde será solicitado todos os dados.
+    execSQLQuery('SELECT * FROM Clientes', res); // ... Chama a função que irá executar a query e retornara os valores.
 }); 
+
+router.get('/clientes/:id?', (req, res) => { // ............................... Define uma rota GET para 'clientes', onde será solicitado apenas um cliente pelo id.
+    let filter = ''; // ....................................................... String que irá receber a query que fará o filtro.
+
+    if(req.params.id) filter = ' WHERE ID = ' + parseInt(req.params.id); // ... Se tiver o id, cria a string com a query para fazer o filtro.
+
+    execSQLQuery('SELECT * FROM Clientes ' + filter, res); // ................. Chama a função que irá executar a query e retornara o valor solicitado, caso ele exista.
+});
+
+
 
 app.use('/', router);
 
